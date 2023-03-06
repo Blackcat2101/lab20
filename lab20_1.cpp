bfccec4128807806afa276c05299461bf0fa86cc
mart <santhod_kh@cmu.ac.th>
2023-03-06 21:29:46 +0700

    lab20_1_ok

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect a,Rect b){
+	double x,y;
+	x = min((a.x+a.w),(b.x+b.w))-max(a.x,b.x);
+	y = min(a.y,b.y)-max((a.y-a.h),(b.y-b.h));
+	if (x>0 && y>0) return x*y;
+	else return 0;
 
+}
 
-
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};	
+	cout << overlap(R1,R2);
 }
